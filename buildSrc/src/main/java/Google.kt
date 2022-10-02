f8457a52a5d6b2462fca6dd089e9a69978d955c4
ashish <ashish.budhathoki@outlook.com>
2022-10-02 15:58:39 +1100

    initial commit

---
+++ buildSrc/src/main/java/Google.kt
@@ -0,0 +1,8 @@
+object Google {
+    private const val materialVersion = "1.4.0"
+    const val material = "com.google.android.material:material:$materialVersion"
+
+    private const val accompanistVersion = "0.24.2-alpha"
+    const val accompanistSwipeRefresh =
+        "com.google.accompanist:accompanist-swiperefresh:$accompanistVersion"
+}
